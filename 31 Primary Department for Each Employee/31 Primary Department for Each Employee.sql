05a3abc71aa6c2541bf3a4afb38e9e5b8a68cfaf
VijayaranganPR <prvijayarangans@gmail.com>
2025-04-23 22:32:19 -0400

    adding 31

---
+++ 31 Primary Department for Each Employee/31 Primary Department for Each Employee.sql
@@ -0,0 +1,14 @@
+﻿with filtered as (select 
+	employee_id, 
+	department_id,
+	ROW_NUMBER () over (partition by employee_id order by primary_flag desc) as rn
+from 
+	Employee)
+
+select 
+	employee_id, 
+	department_id 
+from 
+	filtered
+where 
+	rn = 1
