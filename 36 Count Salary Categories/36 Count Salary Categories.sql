065281f795813963013a8ea9c0104b39c5f3a67a
VijayaranganPR <prvijayarangans@gmail.com>
2025-04-25 23:46:14 -0400

    adding 36

---
+++ 36 Count Salary Categories/36 Count Salary Categories.sql
@@ -0,0 +1,22 @@
+﻿SELECT S.category, COUNT(C.account_id) AS accounts_count FROM (
+	select 'Low Salary' as category
+	union
+	select 'Average Salary' as category
+	union
+	select 'High Salary' as category
+) S
+LEFT JOIN (
+
+SELECT 
+	*, 
+	CASE 
+		WHEN income < 20000 
+			THEN 'Low Salary' 
+		WHEN income <= 50000
+			THEN 'Average Salary'
+		ELSE
+			'High Salary'
+	END AS category
+FROM Accounts) AS C
+ON S.CATEGORY = C.CATEGORY
+GROUP BY S.category
