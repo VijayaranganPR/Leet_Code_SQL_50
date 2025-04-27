1c18a879f155ff44d84614de9b229116ca12989e
VijayaranganPR <prvijayarangans@gmail.com>
2025-04-26 22:30:14 -0400

    adding 44 to 46

---
+++ 44 Fix Names in a Table/44 Fix Names in a Table.sql
@@ -0,0 +1,7 @@
+﻿select 
+	user_id, 
+	upper(substring(name, 1,1)) + lower(SUBSTRING(name, 2, 100)) as name 
+from 
+	users
+order by
+	user_id
---
+++ 45 Patients With a Condition/45 Patients With a Condition.sql
@@ -0,0 +1,2 @@
+﻿select * from patients
+where conditions like 'DIAB1%' or conditions like '% DIAB1%'
