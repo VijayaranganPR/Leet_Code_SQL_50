35b8ec905153bb208e53ed9d91271f2ed15b7c6f
VijayaranganPR <prvijayarangans@gmail.com>
2025-04-27 20:37:13 -0400

    adding 48

---
+++ 48 Group Sold Products By The Date/48 Group Sold Products By The Date.sql
@@ -0,0 +1,13 @@
+﻿select 
+	t.sell_date, 
+	count(product) as num_sold, 
+	STRING_AGG(product, ',') within group (order by product) as products
+from 
+	(select 
+		distinct sell_date, PRODUCT
+	from 
+		Activities) as t
+group by 
+	t.sell_date
+order by
+	t.sell_date
