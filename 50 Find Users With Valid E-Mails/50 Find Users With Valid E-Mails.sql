2e3d7267af8b4384922f0470f719df65bf57cb87
VijayaranganPR <prvijayarangans@gmail.com>
2025-04-27 21:18:37 -0400

    adding 50

---
+++ 50 Find Users With Valid E-Mails/50 Find Users With Valid E-Mails.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+	* 
+FROM 
+	Users
+WHERE 
+	substring(LOWER(MAIL),0, len(mail)-12) not like '%[^a-zA-Z0-9._-]%'  
+	AND substring(LOWER(MAIL),0, len(mail)-12) like '[a-zA-Z]%'
+	AND MAIL LIKE '%leetcode.com' 
