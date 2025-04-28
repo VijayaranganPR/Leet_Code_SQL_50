8d23071a0ffbbfb9fba91c4c3153232dbdfed822
VijayaranganPR <prvijayarangans@gmail.com>
2025-04-27 20:40:17 -0400

    adding 49

---
+++ 49 List the Products Ordered in a Period/49 List the Products Ordered in a Period.sql
@@ -0,0 +1,15 @@
+﻿SELECT 
+	p.product_name, 
+	sum(o.unit) unit 
+FROM
+	Orders o
+left join 
+	products p
+on
+	o.product_id = p.product_id
+WHERE 
+	O.order_date like '2020-02%' 
+group by 
+	p.product_name
+HAVING 
+	SUM(O.unit) >= 100 
